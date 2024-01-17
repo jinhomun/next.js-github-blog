89d66fa9473a6d470399a07f3931a0215b5502cc
jinhomun <answlsgh95@gmail.com>
2024-01-17 11:57:01 +0900

    dsdsds

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /**
  * @type {import('next').NextConfig}
  */
-const isProd = process.env.NODE_ENV = 'production';
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     basePath: isProd ? '/next.js-github-blog' : '',
     output: 'export',
